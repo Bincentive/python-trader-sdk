2b2a844d3235701d483192432aa0cfd0f695ddeb
Stanley Ding <stanley@bincentive.com>
2019-01-21 16:53:46 +0800

    Bump version code to 1.0.0.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='bincentive-trader',
-    version='0.1.0',
+    version='1.0.0',
     author='Bincentive',
     packages=['bincentive_trader'],
     install_requires=[
